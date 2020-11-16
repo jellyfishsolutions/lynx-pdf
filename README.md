3b3800696cf71f0617e248a063d69b17ddf7970b
Luca Roverelli <luca.roverelli@gmail.com>
2020-11-16 18:06:26 +0100

    updated and improved version of the lynx-pdf module

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # lynx-pdf
-Module for the lynx framework to enable the creation of PDF files
+Module for the lynx framework to enable the creation of PDF files.
+
+Requires `lynx-framework@1.1.10` or later versions.
+
+
+## Usage
+
+The module defines static method to generates PDFs.
+
+It is possible to generate a `Media` entity (preferred method):
+
+```
+let media = await PdfModule.generateMediaFromTemplate('templates/pdf/example', context, {}, req.user, pdfDirectory);
+```
+
+or a temporary file:
+
+```
+let path = await PdfModule.generateFromTemplate('templates/pdf/example', context);
+```
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import SimpleModule from "lynx-framework/simple.module";
 import * as puppeteer from "puppeteer";
 import * as fs from "fs";
-import * as uuid from "uuid/v4";
+import { v4 as uuidv4 } from 'uuid';
 import { app } from "lynx-framework/app";
+import Media from "lynx-framework/entities/media.entity";
+import User from "lynx-framework/entities/user.entity";
 
 function compileTemplate(template: string, context: any): Promise<any> {
     return new Promise((res, rej) => {
@@ -58,6 +60,14 @@ export default class PdfModule extends SimpleModule {
     static TMP_FOLDER = __dirname + "/tmp/";
 
     /**
+     * Set the output folder for generated pdf
+     * @param path the new path
+     */
+    static setOutputFolder(path: string) {
+        this.OUTPUT_FOLDER = path;
+    }
+
+    /**
      * Low level method to generate a PDF from a generic file.
      * Probably, you should use the @method generateFromTemplate method.
      * It is just a wraps the puppeteer API.
@@ -100,7 +110,7 @@ export default class PdfModule extends SimpleModule {
             safeMakeDir(PdfModule.OUTPUT_FOLDER)
         ]);
 
-        let generatedHtml = PdfModule.TMP_FOLDER + uuid() + ".html";
+        let generatedHtml = PdfModule.TMP_FOLDER + uuidv4() + ".html";
 
         if (!from.endsWith(".njk")) {
             from += ".njk";
@@ -108,7 +118,7 @@ export default class PdfModule extends SimpleModule {
         let output = await compileTemplate(from, context);
         await createFile(generatedHtml, output);
 
-        let generatedPdf = PdfModule.OUTPUT_FOLDER + uuid() + ".pdf";
+        let generatedPdf = PdfModule.OUTPUT_FOLDER + uuidv4() + ".pdf";
         await PdfModule.generate(
             "file://" + generatedHtml,
             generatedPdf,
@@ -117,4 +127,38 @@ export default class PdfModule extends SimpleModule {
         deleteFile(generatedHtml);
         return generatedPdf;
     }
+
+    /**
+     * Generate a PDF file starting from a nunjuks template and a context.
+     * Finally, a new Media will be created starting from the generated PDF. 
+     * @param from the path of the chosen template
+     * @param context the context to be used to render the chosen template
+     * @param options any options to used by the puppetter library to render the pdf
+     * @param user the optional user performing the request (aka the file owner)
+     * @param directory the optional virtual directory; defaults to '/' (root)
+     * @return the new created media
+     */
+    static async generateMediaFromTemplate(from: string, context: any = {}, options: any = {}, user?: User, directory?: Media): Promise<Media> {
+
+            await safeMakeDir(PdfModule.TMP_FOLDER);
+    
+            let generatedHtml = PdfModule.TMP_FOLDER + uuidv4() + ".html";
+    
+            if (!from.endsWith(".njk")) {
+                from += ".njk";
+            }
+            let output = await compileTemplate(from, context);
+            await createFile(generatedHtml, output);
+    
+            let name = uuidv4() + ".pdf";
+            let generatedPdf = PdfModule.TMP_FOLDER + name;
+            await PdfModule.generate(
+                "file://" + generatedHtml,
+                generatedPdf,
+                options
+            );
+            deleteFile(generatedHtml);
+
+            return Media.persistTempFile(name, generatedPdf, user as any, directory)
+        }
 }
